2fe27659cc94d43f52cc26d1051211fdf91b514c
Adham Sabry <dom.sabry@gmail.com>
2022-06-27 00:17:22 +0000

    feat: added support for hiding progress and custom success/failed messages

--- rules/progress.js
+++ rules/progress.js
@@ -7,10 +7,21 @@ const spinner = ora({
 });
 
 let bindExit = false;
+let initialReportDone = false;
 
-const exitCallback = (exitCode) => {
+const exitCallback = (exitCode, settings) => {
   if (exitCode === 0) {
-    spinner.succeed('Lint done...');
+    spinner.succeed(
+      settings && typeof settings['success-message'] === 'string'
+        ? settings['success-message']
+        : 'Lint done...',
+    );
+  } else {
+    spinner.fail(
+      settings && typeof settings['fail-message'] === 'string'
+        ? settings['fail-message']
+        : 'Lint done with errors.',
+    );
   }
 };
 
@@ -18,15 +29,23 @@ const rootPath = process.cwd();
 
 const create = (context) => {
   if (!bindExit) {
-    process.on('exit', exitCallback);
+    process.on('exit', (code) => {
+      exitCallback(code, context.settings);
+    });
     bindExit = true;
   }
 
-  const filename = context.getFilename();
-  const relativeFilePath = path.relative(rootPath, filename);
+  if (!context.settings['hide-progress']) {
+    const filename = context.getFilename();
+    const relativeFilePath = path.relative(rootPath, filename);
 
-  spinner.text = `Processing: ${chalk.green(relativeFilePath)} \n`;
-  spinner.render();
+    spinner.text = `Processing: ${chalk.green(relativeFilePath)} \n`;
+    spinner.render();
+  } else if (!initialReportDone) {
+    spinner.text = 'Linting \n';
+    spinner.render();
+    initialReportDone = true;
+  }
 
   return {};
 };
